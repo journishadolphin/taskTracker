47cc8c98b0dbd4c75ff1c8481bcab3f0fdc6538f
Journisha Dolphin <journishadolphin@uagateway.org>
2024-03-05 22:31:38 -0500

    Initialize app state variables and cached element references

---
+++ script.js
@@ -0,0 +1,6 @@
+
+// script.js
+// Section 1: TODOs
+// TODO: Register submissions from the user on the form.
+// TODO: Determine the value of the data submitted and add it to a JavaScript array calle
+// TODO: Call the render function to update the table with the new tasks.
